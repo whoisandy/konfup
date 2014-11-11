48cd1af460de2217cc95c40f229f7cf43bf20705
Bhargav Anand <rjn143@gmail.com>
2014-11-11 17:49:10 -0500

    added a few files for testing purpose

--- test/konfup.js
+++ test/konfup.js
@@ -5,66 +5,90 @@ var should = require('should');
 var konflib = require('../'),
     konf = require('../lib/konfup');
 
-var res = null;
+var k, param, res;
 
-describe('Konfup', function(){
-   before(function(){
-       res = konflib;
-   });
+describe('Kconfup ', function(){
 
-    after(function(){
-       res = null;
+    describe('function:', function(){
+        before(function(){
+            k = konflib;
+        });
+
+        after(function(){
+            delete k;
+            delete res;
+        });
+
+        it('should return an instance of konfup and be an object', function(){
+            k.should.be.an.Object;
+            k.should.be.an.instanceOf(konf);
+        });
+
+        it('should expose env, config, boot functions of konfup', function(){
+            k.env.should.be.an.Function;
+            k.config.should.be.an.Function;
+            k.boot.should.be.an.Function;
+        });
+
+        it('should have an initial config which is an empty object', function(){
+            res = k.config();
+            res.should.be.empty;
+        });
     });
 
-   it('should return an instance of konfup and be an object', function(){
-        res.should.be.an.Object;
-        res.should.be.an.instanceOf(konf);
-   });
+    describe('method:', function(){
+        beforeEach(function(){
+            k = konflib;
+        });
 
-   it('should have an initial config which is an empty object', function(){
-      var config = res.config();
-       config.kcon.should.eql.empty;
-   });
+        afterEach(function(){
+            k.kconfig = {};
+            res = undefined;
+            param = undefined;
+        });
 
-   it('should expose env, config, boot functions of konfup', function(){
-       res.env.should.be.an.Function;
-       res.config.should.be.an.Function;
-       res.boot.should.be.an.Function;
-   });
-});
+        it('.env() should return basic env object', function(){
+            res = k.env();
 
-describe('Konfup function', function(){
-   before(function(){
-      res = konflib;
-   });
+            process.env.NODE_ENV.should.eql('dev');
+            process.env.BASIC.should.eql('basic');
+            process.env.SOME_KEY.should.eql('jahbsd76asd867a7sdba87w293b927b9as');
+            process.env.SOME_NAME.should.eql('cooljack');
 
-   it('env should return basic environment object', function(){
-       res.env();
-       process.env.NODE_ENV.should.eql('dev');
-       process.env.BASIC.should.eql('basic');
-       process.env.SOME_KEY.should.eql('jahbsd76asd867a7sdba87w293b927b9as');
-       process.env.SOME_NAME.should.eql('cooljack');
-   });
+            res.kconfig.should.be.an.Object;
+            res.kconfig.should.be.empty;
+        });
 
-   //it('config should return an empty object if no root config file present', function(){
-   //    var config = res.config();
-   //    config.kcon.should.be.empty;
-   //});
+        it('.config() should return an empty object if no root config file present', function(){
+            res = k.config();
+            res.should.be.empty;
+        });
 
-   it('config should return a config object if name given', function(){
-       var config = res.config();
-       config.kcon.should.be.an.Object;
-       config.kcon.should.be.eql(_.merge(config.kcon, require('../config.json')));
-   });
+        it('.config() should return a config object if name given', function(){
+            param = 'settings';
+            res = k.config(param);
+            res.should.be.an.Object;
+            res.should.eql(require('../' + param + '.json'));
+        });
 
-   it('config should return a config object if path given', function(){
-       var configPath = path.resolve(path.join(process.cwd(), 'test', 'config.json'));
-       var config = res.config(configPath);
-       config.kcon.should.be.an.Object;
-       config.kcon.should.eql(_.merge(config.kcon, require('./config.json')));
-   });
+        it('.config() should return a config object if path given', function(){
+            param = path.resolve(path.join(process.cwd(), 'test', 'config.json'));
+            var res = k.config(param);
+            res.should.be.an.Object;
+            res.should.eql(require('./config.json'));
+        });
 
-   it('boot should run two functions', function(){
+        it('.boot() should set env and load empty config', function(){
+            res = k.boot();
 
-   });
+            process.env.NODE_ENV.should.eql('dev');
+            process.env.BASIC.should.eql('basic');
+            process.env.SOME_KEY.should.eql('jahbsd76asd867a7sdba87w293b927b9as');
+            process.env.SOME_NAME.should.eql('cooljack');
+
+            res.should.be.an.Object;
+            res.should.be.empty;
+        });
+    });
+
 });
